890f5de62219b30a014cd98d42bcb966ce40f7ce
Seunghoon Ko <imskojs@gmail.com>
2019-01-10 22:14:42 +0900

    scafold schedule feature module structure

--- src/app/scheduler/scheduler.module.ts
+++ src/app/scheduler/scheduler.module.ts
@@ -2,9 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SchedulerRoutingModule } from './scheduler-routing.module';
+import { SchedulerComponent } from './scheduler.component';
+import { ControlComponent } from './control/control.component';
+import { CalendarComponent } from './calendar/calendar.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SchedulerComponent,
+    ControlComponent,
+    CalendarComponent
+  ],
   imports: [
     CommonModule,
     SchedulerRoutingModule
