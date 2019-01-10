a4776a5bd85e731e7fcda15c64d2378624c6d2ec
Seunghoon Ko <imskojs@gmail.com>
2019-01-10 21:51:01 +0900

    create scheduler feature module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SchedulerModule } from './scheduler/scheduler.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SchedulerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/scheduler/scheduler.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SchedulerRoutingModule } from './scheduler-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SchedulerRoutingModule
+  ]
+})
+export class SchedulerModule { }
