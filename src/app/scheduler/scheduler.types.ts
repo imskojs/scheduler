faca2426aa5ca0f1c80c32ede2023c0fc45c313d
Seunghoon Ko <imskojs@gmail.com>
2019-01-12 08:50:36 +0900

    [types] define simple date type

---
+++ src/app/scheduler/scheduler.types.ts
@@ -0,0 +1,10 @@
+type DaysOfWeek = 'monday' | 'tuesday' | 'wednesday' | 'thursday' |
+  'friday' | 'saturday' | 'sunday';
+
+export class SimpleDate {
+  year: number;
+  month: number;
+  day: number;
+  daysOfWeek: DaysOfWeek;
+}
+
