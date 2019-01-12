c7b1edec2c11a283c3072b797a3d8e330491ac3e
Seunghoon Ko <imskojs@gmail.com>
2019-01-12 19:09:40 +0900

    add unnecessarily nested routes for fun

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SchedulerComponent } from './scheduler/scheduler.component';
+import { CalendarComponent } from './scheduler/calendar/calendar.component';
+import { CalendarMonthlyComponent } from './scheduler/calendar-monthly/calendar-monthly.component';
+import { CalendarWeeklyComponent } from './scheduler/calendar-weekly/calendar-weekly.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'scheduler', pathMatch: 'full' },
-  { path: 'scheduler', component: SchedulerComponent }
+  {
+    path: 'scheduler', component: SchedulerComponent,
+    children: [
+      { path: '', redirectTo: 'calendar', pathMatch: 'full' },
+      {
+        path: 'calendar', component: CalendarComponent,
+        children: [
+          { path: '', redirectTo: 'month', pathMatch: 'full' },
+          { path: 'month', component: CalendarMonthlyComponent },
+          { path: 'week', component: CalendarWeeklyComponent }
+        ]
+      }
+    ]
+  }
 ];
 
 @NgModule({
