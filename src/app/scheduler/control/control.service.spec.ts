8315a01eee8dbc9cd05711c3139c9f23a45f4f93
Seunghoon Ko <imskojs@gmail.com>
2019-01-12 11:27:25 +0900

    implement selectDate and selectedDate method for controlService

--- src/app/scheduler/control/control.service.spec.ts
+++ src/app/scheduler/control/control.service.spec.ts
@@ -1,14 +1,54 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ControlService } from './control.service';
+import { SchedulerService } from '../scheduler.service';
+import { SimpleDateTime } from '../scheduler.types';
+const {toSimpleDateTime} = SchedulerService;
 
 describe('ControlService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    providers: [ControlService]
-  }));
+  let service: ControlService;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [ControlService]
+    });
+    service = TestBed.get(ControlService);
+  });
 
   it('should be created', () => {
-    const service: ControlService = TestBed.get(ControlService);
     expect(service).toBeTruthy();
   });
+  describe('#getSelectedDate method', () => {
+    it('should return observable which emits SimpleDateTime on subscribe', () => {
+      const selectedDate$ = service.getSelectedDate();
+      const simpleDateTimeNow = toSimpleDateTime(Date.now());
+      selectedDate$.subscribe((date: SimpleDateTime) => {
+        expect(date).toEqual(simpleDateTimeNow);
+      });
+    });
+
+    it('should return observable which emits the latest selected SimpleDateTime on subscribe', () => {
+      const selectedDate$ = service.getSelectedDate();
+      const simpleDateTime = toSimpleDateTime(1547249260794);
+      const subscription = service.getSelectedDate().subscribe();
+      service.selectDate(simpleDateTime);
+      subscription.unsubscribe();
+      service.getSelectedDate().subscribe((date: SimpleDateTime) => {
+        expect(date).toEqual(simpleDateTime);
+      });
+    });
+  });
+
+  describe('#selectDate', () => {
+    it('should emit passed in data to an observable created by #getSelectedDate method', () => {
+      const selectedDate$ = service.getSelectedDate();
+      const simpleDateTime = toSimpleDateTime(1547249260794);
+      let latestSimpleDateTime: SimpleDateTime;
+      service.getSelectedDate().subscribe((date: SimpleDateTime) => {
+        latestSimpleDateTime = date;
+      });
+      service.selectDate(simpleDateTime);
+      expect(latestSimpleDateTime).toEqual(simpleDateTime);
+    });
+  });
+
 });
--- src/app/scheduler/control/control.service.ts
+++ src/app/scheduler/control/control.service.ts
@@ -1,7 +1,28 @@
 import { Injectable } from '@angular/core';
+import { Subject, Observable } from 'rxjs';
+import { SimpleDateTime } from '../scheduler.types';
+import { startWith, shareReplay } from 'rxjs/operators';
+
+import { SchedulerService } from '../scheduler.service';
+const { toSimpleDateTime } = SchedulerService;
 
 @Injectable()
 export class ControlService {
 
+  private selectedDateSub: Subject<SimpleDateTime> = new Subject();
+
+  private selectedDate$: Observable<SimpleDateTime> = this.selectedDateSub.asObservable().pipe(
+    startWith(toSimpleDateTime(Date.now())),
+    shareReplay(1)
+  );
+
   constructor() { }
+
+  public getSelectedDate(): Observable<SimpleDateTime> {
+    return this.selectedDate$;
+  }
+
+  public selectDate(simpleDateTime: SimpleDateTime): void {
+    this.selectedDateSub.next(simpleDateTime);
+  }
 }
