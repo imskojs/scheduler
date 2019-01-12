ea3ce745b9a50ed0811647d4d6e8110a9cbf4a42
Seunghoon Ko <imskojs@gmail.com>
2019-01-12 19:20:36 +0900

    [refactor] contain routing logic for feature module within feature module routing

--- src/app/scheduler/scheduler-routing.module.ts
+++ src/app/scheduler/scheduler-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CalendarMonthlyComponent } from './calendar-monthly/calendar-monthly.component';
+import { CalendarWeeklyComponent } from './calendar-weekly/calendar-weekly.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { SchedulerComponent } from './scheduler.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: SchedulerComponent,
+    children: [
+      { path: '', redirectTo: 'calendar', pathMatch: 'full' },
+      {
+        path: 'calendar', component: CalendarComponent,
+        children: [
+          { path: '', redirectTo: 'month', pathMatch: 'full' },
+          { path: 'month', component: CalendarMonthlyComponent },
+          { path: 'week', component: CalendarWeeklyComponent }
+        ]
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
