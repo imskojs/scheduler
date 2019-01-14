4df5dd2b1243ef646283543f50638c0039d487e0
Seunghoon Ko <imskojs@gmail.com>
2019-01-14 21:47:44 +0900

    add in memory db and do sanity check

--- express-server.js
+++ express-server.js
@@ -1,8 +1,17 @@
 const app = require('express')();
 const port = 3000;
+const loki = require('lokijs')
+const db = new loki();
+const schedules = db.addCollection('schedules');
+// $loki is ID
+schedules.insert({month: 1})
+schedules.insert({month: 1})
+schedules.insert({month: 1})
+schedules.insert({month: 1})
 
 app.get('/schedule', (req, res) => {
-  return res.send('data sent')
+  const data = schedules.find({month: 1});
+  return res.send(data)
 });
 
 app.listen(port, () => console.log(`expres running at http://localhost:${port}`));
